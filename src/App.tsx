4cd5008dad54e3cea774e379b34e493c0dc352ea
Azamat Malikov <crazy-zam@ya.ru>
2024-04-08 11:29:34 +0600

    configure Vite, Prettier, ESLint, basic Layout

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <div>Hello world!!!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = (name, id) => {
+  return <div>{name}</div>;
+};
+
+export default Test;
