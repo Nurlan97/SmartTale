714bbbf1e981b6a408c163db41e99cbac37b3578
Azamat Malikov <crazy-zam@ya.ru>
2024-04-26 10:40:12 +0600

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,40 @@ import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 import styles from './App.module.scss';
+import AuthRoute from './components/AuthRoute/AuthRoute';
 import NavBar from './components/NavBar/NavBar';
 import EquipmentPage from './pages/EquipmentPage/EquipmentPage';
 import PlaceOrderPage from './pages/PlaceOrderPage/PlaceOrderPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import ServicesPage from './pages/ServicesPage/ServicesPage';
-import { userStore } from './store';
 
 const App = observer(() => {
   const location = useLocation();
   const [showNavbar, setShowNavbar] = useState(true);
 
   useEffect(() => {
-    // Список путей, на которых навбар не должен отображаться
+    // Список путей, на которых навбар не должен отображаться, это страницы авторизаций
     const noNavbarRoutes = ['/registration', '/autorization'];
-
     setShowNavbar(!noNavbarRoutes.includes(location.pathname));
   }, [location]);
+
   return (
-    <>
-      {showNavbar && (
-        <div className={styles.withNavbar}>
-          <NavBar />
-          <div>navbar will be here</div>
-          <div className={styles.page}>
-            <Routes>
-              <Route path='/equipment' element={<EquipmentPage />} />
-              <Route path='/services' element={<ServicesPage />} />
-              <Route path='/place-order' element={<PlaceOrderPage />} />
-            </Routes>
-          </div>
-        </div>
-      )}
-      {!userStore.isAuth && (
-        <div className={styles.auth}>
-          <Routes>
+    <div className={showNavbar ? styles.withNavbar : styles.auth}>
+      {showNavbar && <NavBar />}
+      {showNavbar && <div>navbar will be here</div>}
+      <div className={showNavbar ? styles.page : styles.auth}>
+        <Routes>
+          <Route path='/equipment' element={<EquipmentPage />} />
+          <Route path='/services' element={<ServicesPage />} />
+          <Route path='/place-order' element={<PlaceOrderPage />} />
+          <Route element={<AuthRoute />}>
+            {/*Сюда пихаем роуты до авторизации, чтобы страницы с авторизацией не были доступны авторизованному пользователю*/}
             <Route path='/registration' element={<RegistrationPage />}></Route>
             {/* <Route path='/autorization' element={<AuthorizationPAge />} /> */}
-          </Routes>
-        </div>
-      )}
-    </>
+          </Route>
+        </Routes>
+      </div>
+    </div>
   );
 });
 
